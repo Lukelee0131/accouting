50df5f4e24b19a9c83fcfee548057b6ab000e12e
Lukelee0131 <Luke.lee0131@gmail.com>
2017-11-19 15:11:30 +0800

    Add files via upload

--- js/lookup.js
+++ js/lookup.js
@@ -6,8 +6,8 @@ accountingCollection.load();
 
 
 function Search() {
+    $("#lookupTable2").find("tr").remove();
     if ($('input:checked').val() == "curMonth") {
-        $("#lookupTable2").find("tr").remove();
         var date = new Date();//new 一個叫做Date的物件
         var year = date.getUTCFullYear();// 取得當前的年份,2017_\
         var month = date.getUTCMonth() + 1;// 取得當前月份
@@ -21,31 +21,55 @@ function Search() {
             {
                 date: {
                     $gte: dateString
-                }     
+                }
             },
             {
-                $orderBy: {"date": -1}
+                $orderBy: { "date": -1 }
             }
         );
+        var eatCost = 0;
+        var playCost = 0;
+        var otherCost = 0;
+        var totalCost = 0;
         for (var i = 0; i < accountings.length; i++) {
             var date = accountings[i].date;
             var category = accountings[i].category;
             var item = accountings[i].item;
             var cost = accountings[i].cost;
+            var id = accountings[i]._id;
+
             $("#lookupTable2").append(
                 "<tr><td>" + date +
                 "</td><td>" + category +
                 "</td><td>" + item +
                 "</td><td>" + cost +
+                "</td><td>" + "<button class=\"btn btn-danger little\" onclick=\"remove('"+id+"')\">刪除</button>" +
                 "</td></tr>");
+            if (accountings[i].category == "吃的") {
+                eatCost += accountings[i].cost / 1
+                console.log(eatCost)
+            }
+            if (accountings[i].category == "玩的") {
+                playCost += accountings[i].cost / 1
+                console.log(playCost)
+            }
+            if (accountings[i].category == "其他") {
+                otherCost += accountings[i].cost / 1
+                console.log(otherCost)
+            }
         }
+        totalCost = eatCost + playCost + otherCost;
     } else {
+        var eatCost = 0;
+        var playCost = 0;
+        var otherCost = 0;
+        var totalCost = 0;
         var accountings = accountingCollection.find(
             {
                 date: {
                     $gte: $("#fromTime").val(),
                     $lte: $("#toTime").val()
-                }     
+                }
             },
             {
                 $orderBy: {
@@ -58,14 +82,44 @@ function Search() {
             var category = accountings[i].category;
             var item = accountings[i].item;
             var cost = accountings[i].cost;
+            var id = accountings[i]._id;
+
             $("#lookupTable2").append(
                 "<tr><td>" + date +
                 "</td><td>" + category +
                 "</td><td>" + item +
                 "</td><td>" + cost +
+                "</td><td>" + "<button class=\"btn btn-danger little\" onclick=\"remove('"+id+"')\">刪除</button>" +
                 "</td></tr>");
+            if (accountings[i].category == "吃的") {
+                eatCost += accountings[i].cost / 1
+                console.log(eatCost)
+            }
+            if (accountings[i].category == "玩的") {
+                playCost += accountings[i].cost / 1
+                console.log(playCost)
+            }
+            if (accountings[i].category == "其他") {
+                otherCost += accountings[i].cost / 1
+                console.log(otherCost)
+            }
         }
-    }
+        totalCost = eatCost + playCost + otherCost;
 
-};
+    }
+    $("#eatCost").text(eatCost)
+    $("#playCost").text(playCost)
+    $("#otherCost").text(otherCost)
+    $("#totalCost").text(totalCost)
+    $("#eatper").text(Math.round(eatCost/totalCost*100)+"%")
+    $("#playper").text(Math.round(playCost/totalCost*100)+"%")
+    $("#otherper").text(Math.round(otherCost/totalCost*100)+"%")
+}
+function remove(id){
+       accountingCollection.remove({
+           _id: id
+       });
+       accountingCollection.save();
+       Search()
+    }
 
